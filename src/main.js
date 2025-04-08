9301b956902ca4d80efaaefd7a9cb248453d7599
GoDuHwan <rhenghks5167@naver.com>
2025-04-08 17:40:07 +0900

    chore: fontawesome 관련 install

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// import font awesome icon component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// import specific icons
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+// add icons to the library
+library.add(fas, far, fab)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app
+  .component('font-awesome-icon', FontAwesomeIcon) // <-- FontAwesome Component 추가
+  .mount('#app')
