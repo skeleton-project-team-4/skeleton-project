2fc0a6eb1232b94eaf2f17cb121b4e87e74d2431
hyunseo <wndlthsk@naver.com>
2025-04-10 15:45:55 +0900

    feat: 홈 화면 합치기

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,16 @@
-<script setup></script>
-<template></template>
+<template>
+  <Header class="header" />
+  <router-view />
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue'
+</script>
+
+<style scopted>
+.header {
+  width: 1440px;
+  height: 105px;
+  margin: 0 auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,2 +1,52 @@
-<script setup></script>
-<template></template>
+<template>
+  <div class="content">
+    <div class="recent-monthly">
+      <div class="recent">
+        <RecentHistoryList />
+      </div>
+      <div class="monthly">
+        <MonthHistory />
+      </div>
+    </div>
+    <div class="donut-chart">
+      <MonthHistoryForCategory />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import RecentHistoryList from '@/components/RecentHistoryList.vue'
+import MonthHistory from '@/components/MonthHistory.vue'
+import MonthHistoryForCategory from '@/components/MonthHistoryForCategory.vue'
+</script>
+
+<style scoped>
+.content {
+  width: 1380px;
+  height: 100%;
+  margin: 0 auto 50px;
+}
+
+.recent-monthly {
+  margin-top: 25px;
+  width: 1380px;
+  height: 550px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+.recent {
+  width: 450px;
+  height: 550px;
+  background-color: #faabab;
+}
+
+.donut-chart {
+  margin-top: 30px;
+  width: 1380px;
+  height: 823px;
+  background-color: #faabab;
+}
+</style>
