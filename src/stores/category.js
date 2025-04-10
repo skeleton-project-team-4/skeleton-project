ada88944271c20c92347cc36b07ab74df20a63a8
sunbread8355 <sunbread8355@gmail.com>
2025-04-10 11:19:14 +0900

    fix:카테고리 스토어 변경

--- src/stores/category.js
+++ src/stores/category.js
@@ -6,6 +6,19 @@ export const useCategoryStore = defineStore('category', () => {
     expense: 'expenseCategory',
     income: 'incomeCategory',
   }
+  const fetchCategory = async (type) => {
+    const requestCategory = category[type]
+    if (!requestCategory) {
+      alert('지원하지 않는 형식입니다')
+      return
+    }
+    try {
+      const response = await axios.get(`/api/${requestCategory}`)
+      return response.data
+    } catch (error) {
+      console.error('카테고리 데이터 로드 실패')
+    }
+  }
 
   const fetchIcon = async (type, name) => {
     const requestCategory = category[type]
@@ -20,5 +33,5 @@ export const useCategoryStore = defineStore('category', () => {
       console.error('아이콘 로드 실패')
     }
   }
-  return { fetchIcon }
+  return { fetchCategory, fetchIcon }
 })
