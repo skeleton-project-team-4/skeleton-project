40ef32086840e473e4068bf188336620d1fa37c0
조아빈 <whdkqls122@naver.com>
2025-04-11 11:59:53 +0900

    fix : 금액 15자리 보다 큰 금액 입력 시 null로 초기화 하도록 수정 - 15자리보다 커도 에러메시지는 출력되지만 저장이 되는 문제 발생 - 문제 발생 시 null로 초기화 하여 수정

--- src/components/TransactionModal.vue
+++ src/components/TransactionModal.vue
@@ -340,6 +340,7 @@ const amountText = computed({
 
     if (/^[1-9]\d*$/.test(raw)) {
       if (raw.length > 15) {
+        newTransaction.amount = null
         errorMsg.amount = '금액은 15자리 이하만 입력 가능합니다.'
       } else {
         newTransaction.amount = parseInt(raw)
@@ -364,6 +365,7 @@ const EditAmountText = computed({
 
     if (/^[1-9]\d*$/.test(raw)) {
       if (raw.length > 15) {
+        transaction.value.amount = null
         errorMsg.amount = '금액은 15자리 이하만 입력 가능합니다.'
       } else {
         transaction.value.amount = parseInt(raw)
